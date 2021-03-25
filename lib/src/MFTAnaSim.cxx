8d2b2653aceb3d90b6451f90a332376ad9d8fe03
Bogdan Vulpescu <Bogdan.Vulpescu@clermont.in2p3.fr>
2021-03-25 15:44:58 +0100

    Add count MC particles

--- lib/src/MFTAnaSim.cxx
+++ lib/src/MFTAnaSim.cxx
@@ -1,5 +1,7 @@
 #include "../include/MFTAnaSim.h"
 
+#include "TDatabasePDG.h"
+
 namespace o2::mftana
 {
 
@@ -18,11 +20,13 @@ void MFTAnaSim::initialize(Int_t maxMCTracks)
   mNrEvents = mKineTree->GetEntries();
   printf("Number of generated events: %d \n", mNrEvents);
   mHitTree->SetBranchAddress("MFTHit",&mHitVecP);
+
+  // maximum MC tracks per event, from a scan of all events in the file
   mMCTrackHasHitsInDisk = std::vector<std::array<bool, o2::mft::constants::DisksNumber>>(maxMCTracks, {0, 0, 0, 0, 0});
 }
 
 //_____________________________________________________________________________
-void MFTAnaSim::initEvent(Int_t event, Int_t nMCTracks)
+void MFTAnaSim::initEvent(Int_t event, Int_t nMCTracks, Int_t particleSource)
 {
   mCurrEvent = event;
   mNrMCTracks = nMCTracks;
@@ -30,6 +34,77 @@ void MFTAnaSim::initEvent(Int_t event, Int_t nMCTracks)
     for (auto d = 0; d < o2::mft::constants::DisksNumber; d++) {
       mMCTrackHasHitsInDisk.at(i)[d] = false;
     }
+  }
+  mParticles.clear();
+  switch (particleSource) {
+  case 0:
+    mPrimary = kTRUE;
+    mSecondary = kFALSE;
+    mAll = kFALSE;
+    break;
+  case 1:
+    mPrimary = kFALSE;
+    mSecondary = kTRUE;
+    mAll = kFALSE;
+    break;
+  case 2:
+    mPrimary = kFALSE;
+    mSecondary = kFALSE;
+    mAll = kTRUE;
+    break;
+  default:
+    mPrimary = kFALSE;
+    mSecondary = kFALSE;
+    mAll = kFALSE;
+    break;
+  };
+	
+}
+
+//_____________________________________________________________________________
+Bool_t MFTAnaSim::doParticles()
+{
+  MCPart particle;
+  Int_t pdgCode;
+  for (Int_t trkID = 0 ; trkID < mNrMCTracks; trkID++) {
+    MCTrack* mcTrack =  &(mMCTrkVec)[trkID];
+    if (!mAll) {
+      if (mPrimary && !mcTrack->isPrimary()) {
+	continue;
+      }
+      if (mSecondary && mcTrack->isPrimary()) {
+	continue;
+      }
+    }
+    pdgCode = mcTrack->GetPdgCode();
+    // skip pomeron and reggeon
+    if (pdgCode == 110 || pdgCode == 990) {
+      continue;
+    }
+    // skip ...
+    if (pdgCode > 990000000) {
+      continue;
+    }
+    // diffractive states
+    if (pdgCode > 9900000) {
+      pdgCode -= 9900000;
+    }
+    //printf("MCTrack %d \n", trkID);
+    //printf("pdg code: %d \n", pdgCode);
+    //printf("name: %s \n", TDatabasePDG::Instance()->GetParticle(pdgCode)->GetName());
+    
+    countParticle(pdgCode);
+    /*
+    if (mCurrEvent == 0) {
+      printf("MCTrack %4d   PDG %4d   name %s   isSec %d   E %7.3f \n",
+	     trkID,
+	     pdgCode,
+	     TDatabasePDG::Instance()->GetParticle(pdgCode)->GetName(),
+	     mcTrack->isSecondary(),
+	     mcTrack->GetEnergy());
+      printf("Particle name: %s \n", particle.mPDGName.c_str());
+    }
+    */
   }
 }
 
@@ -68,4 +143,26 @@ Bool_t MFTAnaSim::doMCTracks()
   return kTRUE;
 }
 
+//_____________________________________________________________________________
+void MFTAnaSim::countParticle(Int_t pdgCode)
+{
+  MCPart newPart;  
+  MCPart part;
+  Bool_t counted = kFALSE;
+  for (Int_t i = 0; i < mParticles.size(); i++) {
+    part = mParticles.at(i);
+    if (part.mPDGCode == pdgCode) {
+      part.mCount++;
+      counted = kTRUE;
+      break;
+    }
+  }
+  if (!counted) {
+    newPart.mPDGCode = pdgCode;
+    newPart.mPDGName = std::string(TDatabasePDG::Instance()->GetParticle(pdgCode)->GetName());
+    newPart.mCount = 1;
+    mParticles.push_back(newPart);
+  }
+}
+  
 } // end namespace o2::mftana
