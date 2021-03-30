0249b192ecc8bd39b877b6625d85461d8e38ac88
Bogdan Vulpescu <Bogdan.Vulpescu@clermont.in2p3.fr>
2021-03-30 15:27:46 +0200

    To complete previous commit

--- lib/src/MFTAnaSimLinkDef.h
+++ lib/src/MFTAnaSimLinkDef.h
@@ -5,6 +5,13 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
-//#pragma link C++ class o2::mftana::MFTAnaHits + ;
+#pragma link C++ class o2::mftana::MFTAnaSimHit+;
+#pragma link C++ class o2::mftana::MFTAnaSimTrack+;
+#pragma link C++ class o2::mftana::MFTAnaSimCluster+;
+#pragma link C++ class o2::mftana::MFTAnaMCTrack+;
+#pragma link C++ class o2::mft::Cluster+;
+#pragma link C++ class std::vector<o2::mftana::MFTAnaSimHit>+;
+#pragma link C++ class std::vector<o2::mftana::MFTAnaSimCluster>+;
+#pragma link C++ class std::vector<o2::mftana::MFTAnaSimTrack>+;
 
 #endif /* __CLING__ */
