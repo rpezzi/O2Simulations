b91b58fbc2b482b3d40ba0697e9f0f5c67bedb78
Bogdan Vulpescu <Bogdan.Vulpescu@clermont.in2p3.fr>
2021-06-09 16:10:13 +0200

    Add doxygen documentation; other fixes

--- lib/include/MFTAnaSim.h
+++ lib/include/MFTAnaSim.h
@@ -1,3 +1,5 @@
+/// Main class for the analysis of Muon Forward Tracker simulations
+
 #ifndef MFT_ANA_SIM
 #define MFT_ANA_SIM
 
@@ -39,66 +41,74 @@ class MFTAnaSim
   MFTAnaSim();
   ~MFTAnaSim() = default;
 
-  int mNEvents = 0;
-
-  TTree* mHitTree = nullptr;
-  TTree* mKineTree = nullptr;
-  TTree* mClusTree = nullptr;
-  TTree* mTrackTree = nullptr;
+  int mNEvents = 0;             ///< Number of events in the input files
+  TTree* mHitTree = nullptr;    ///< Tree with hits, set from the steering macro
+  TTree* mKineTree = nullptr;   ///< Tree with particle kinematics, set from the steering macro
+  TTree* mClusTree = nullptr;   ///< Tree with clusters, set from the steering macro
+  TTree* mTrackTree = nullptr;  ///< Tree with standalone reconstructed tracks, set from the steering macro
   
-  o2::mft::GeometryTGeo* mGeoManager;
-  o2::itsmft::ChipMappingMFT mChipMapper;
+  o2::mft::GeometryTGeo* mGeoManager;      ///< Manager of the MFT geometry
+  o2::itsmft::ChipMappingMFT mChipMapper;  ///< Information on the MFT chip mapping
 
-  bool initialize(int maxMCTracks);
-  void initEvent(int event, int nMCTracks, int particleSource = 0);
-  bool doParticles();
-  bool doHits();
-  bool doMCTracks();
-  bool doSATracks();
-  void finishEvent();
-  void finish();
-  void findMCTrackHits(int trkID, int& firstIndex, int& lastIndex);
-  void findMCTrackClusters(int trkID, int& firstIndex, int& lastIndex);
+  bool initialize(int maxMCTracks);   ///< Global initialization for all events
+  void initEvent(int event, int nMCTracks, int particleSource = 0); ///< Event initialization
+  bool doParticles();   ///< Extract information of all particles generated in one event, may select primaries and/or secondaries
+  bool doHits();   ///< Count the hits per layer and disk
+  bool doMCTracks();   ///< Extract information of the Monte-Carlo tracks in a vector of MFTAnaSimTrack
+  bool doSATracks();   ///< Extract information of the standalone reconstructed tracks in a vector of MFTAnaSimSATrack
+  void finishEvent();   ///< Called after each event
+  void finish();   ///< Called at the end
+  void findMCTrackHits(int trkID, int& firstIndex, int& lastIndex);   ///< Return the index range in the vector of all extracted hits (MFTAnaSimHit) for a given MC track
+  void findMCTrackClusters(int trkID, int& firstIndex, int& lastIndex);   ///< Return the index range in the vector of all extracted clusters (MFTAnaSimCluster) for a given MC track
   void setVerboseLevel(int vl) { mVerboseLevel = vl; }
-  const std::vector<MCPart>& getParticles() { return mParticles; }
+  const std::vector<MCPart>& getParticles() { return mParticles; }   ///< Return the vector of (MCPart) extracted particles in the event
 
+  struct ClusterCoord {
+    float xGlo = 0.;
+    float yGlo = 0.;
+    float zGlo = 0.;
+    int nPixels = 0;
+  };
+  
  private:
-  bool mPrimary = kTRUE, mSecondary = kFALSE, mAll = kFALSE;
+  bool mPrimary = true, mSecondary = false, mAll = false;
   enum ParticleSource {kPrimary, kSecondary, kAll};
-  void filterPDGCode(int& pdgCode);
-  void countParticle(int pdgCode);
+  void filterPDGCode(int& pdgCode);   ///< Skip some PDG codes
+  void countParticle(int pdgCode);   ///< Count the particle species and store in a vector of MCPart
+  void extractClustersCoord();
   
-  std::vector<o2::MCTrack> mMCTrkVec, *mMCTrkVecP = &mMCTrkVec;
-  std::vector<o2::itsmft::Hit> mHitVec, *mHitVecP = &mHitVec;
-  std::vector<o2::itsmft::CompClusterExt> mClusVec, *mClusVecP = &mClusVec;
-  std::vector<unsigned char> mClusPatterns, *mClusPatternsP = &mClusPatterns;
-  std::vector<o2::mft::TrackMFT> mTrackVec, *mTrackVecP = &mTrackVec;
-  o2::dataformats::MCTruthContainer<o2::MCCompLabel>* mClusLabels = nullptr;
-  std::vector<o2::MCCompLabel> mTrackLabels, *mTrackLabelsInp = &mTrackLabels;
-  std::vector<int> mTrackExtClsVec, *mTrackExtClsVecP = &mTrackExtClsVec;
+  std::vector<o2::MCTrack> mMCTrkVec, *mMCTrkVecP = &mMCTrkVec;   ///< Vector of extracted MCTrack
+  std::vector<o2::itsmft::Hit> mHitVec, *mHitVecP = &mHitVec;   ///< Vector of hits
+  std::vector<o2::itsmft::CompClusterExt> mClusVec, *mClusVecP = &mClusVec;   ///< vector of clusters
+  std::vector<unsigned char>* mClusPatternsP = nullptr;   ///< vector of cluster patterns
+  std::vector<o2::mft::TrackMFT> mTrackVec, *mTrackVecP = &mTrackVec;   ///< Vector of standalone reconstructed o2::mft::TrackMFT
+  o2::dataformats::MCTruthContainer<o2::MCCompLabel>* mClusLabels = nullptr;   ///< MC labels for clusters
+  std::vector<o2::MCCompLabel> mTrackLabels, *mTrackLabelsInp = &mTrackLabels;   ///< MC labels for the standalone reconstructed tracks
+  std::vector<int> mTrackExtClsVec, *mTrackExtClsVecP = &mTrackExtClsVec;   ///< External index for the clusters associated to the standalone reconstructed tracks
 
-  std::vector<std::array<bool, o2::mft::constants::DisksNumber>> mMCTrackHasHitsInDisk;
-  std::vector<std::array<bool, o2::mft::constants::LayersNumber>> mMCTrackHasHitsInLayer;
-  std::vector<MCPart> mParticles;
-  std::vector<MFTAnaSimTrack> mAnaSimTracks;
-  std::vector<MFTAnaSimCluster> mAnaSimClusters;
-  std::vector<MFTAnaSimHit> mAnaSimHits;
-  std::vector<std::pair<int, int>> mTrackIDtoMC;
+  std::vector<std::array<bool, o2::mft::constants::DisksNumber>> mMCTrackHasHitsInDisk;   ///< Counting MFT disks with hits
+  std::vector<std::array<bool, o2::mft::constants::LayersNumber>> mMCTrackHasHitsInLayer;   ///< Counting MFT layers with hits
+  std::vector<MCPart> mParticles;   ///< Vector counting the generated particles 
+  std::vector<MFTAnaSimTrack> mAnaSimTracks;   ///< Vector with extracted MC tracks
+  std::vector<MFTAnaSimCluster> mAnaSimClusters;   ///< Vector with clusters associated to MFTAnaSimTrack
+  std::vector<MFTAnaSimHit> mAnaSimHits;   ///< Vector with hits associated to MFTAnaSimTrack
 
-  o2::itsmft::TopologyDictionary mTopoDict;
+  o2::itsmft::TopologyDictionary mTopoDict;   ///< Dictionary for the cluster topologies
+
+  std::vector<ClusterCoord> mClustersCoord;   ///< Simple structure to store cluster coordinates
  
-  TFile* mOutFile = nullptr;
+  TFile* mOutFile = nullptr;    ///< Output ROOT file
   
-  TTree* mOutTree1 = nullptr;   // MFTAnsSimTrack
-  TTree* mOutTree2 = nullptr;   // MFTAnaSimHit
-  TTree* mOutTree3 = nullptr;   // MFTAnaSimCluster 
+  TTree* mOutTree1 = nullptr;   ///< Tree of MFTAnaSimTrack in the output file
+  TTree* mOutTree2 = nullptr;   ///< Tree of MFTAnaSimHit in the output file
+  TTree* mOutTree3 = nullptr;   ///< Tree of MFTAnaSimCluster in the output file 
 
-  int mCurrEvent = 0;
-  int mNMCTracks = 0;
-  int mNSATracks = 0;
-  int mMaxMCTracks = 0;
-  int mNHitsInEvent = 0;
-  int mNClusters = 0;
+  int mCurrEvent = 0;     ///< Index of the current event
+  int mNMCTracks = 0;     ///< Number of MC tracks in event
+  int mNSATracks = 0;     ///< Number of standalone reconstructed tracks for all events in the input file
+  int mMaxMCTracks = 0;   ///< Maximum number of MC tracks per event
+  int mNHitsInEvent = 0;  ///< Number of hits in event
+  int mNClusters = 0;     ///< Number of clusters for all events in the input file
   int mVerboseLevel = 0;
 };
   
